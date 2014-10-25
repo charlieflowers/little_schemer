2c64731b9ba18c956a2e59f62d8c147d1082365e
charlieflowers <cflowers@gmail.com>
2014-10-24 23:28:44 -0400

    Stuck on mysterious stackoverflow error. () is not longer considered an empty list.

--- src/little_schemer/core.clj
+++ src/little_schemer/core.clj
@@ -10,11 +10,20 @@
   (fn [a]
     (not (seq? a))))
 
+;; (def null?
+;;   (fn [a]
+;;     (println "checking if " a " is empty list or nil")
+;;     (or
+;;      (nil? a)
+;;      (empty? a))))
+;;
+;; I was getting a stackoverflow as if null? had quit working, WAAY more than half way through the book. So I googled,
+;;  and (seq coll) is the idiomatic was to check for empty list in clojure. So changing null? to do that.
+;; It DID NOT HELP THE STACKOVERFLOW ONE FUCKING BIT.
+
 (def null?
-  (fn [a]
-    (or
-     (nil? a)
-     (= () a))))
+  (fn [xs]
+    (seq xs)))
 
 (def slist?
   (complement atom?))
@@ -315,3 +324,225 @@
                          (eqan? (first xs) a) (add1 (occur* a (rest xs)))
                          :else (occur* a (rest xs)))
      :else (plus (occur* a (first xs)) (occur* a (rest xs))))))
+
+(def subst*
+  (fn [new old xs]
+    (cond
+     (null? xs) ()
+     (atom? (first xs)) (cond
+                         (eqan? (first xs) old) (cons new (subst* new old (rest xs)))
+                         :else (cons (first xs) (subst* new old (rest xs))))
+     :else (cons (subst* new old (first xs)) (subst* new old (rest xs))))))
+
+(def insertL*
+  (fn [new old xs]
+    (cond
+     (null? xs) ()
+     (atom? (first xs)) (cond
+                         (eqan? (first xs) old) (cons new (cons old (insertL* new old (rest xs))))
+                         :else (cons (first xs) (insertL* new old (rest xs))))
+     :else (cons (insertL* new old (first xs)) (insertL* new old (rest xs))))))
+
+(def member*             ; Sweet! Mine differs from his. At first, I thought mine was wrong, but it's RIGHT (his is too).
+  (fn [a xs]             ; He uses an OR instead of COND right ...
+    (cond
+     (null? xs) false
+     (atom? (first xs)) (cond
+                         (eqan? (first xs) a) true      ; ... HERE! But it has exactly the same effect. Because the result is boolean.
+                         :else (member* a (rest xs)))
+     :else (or (member* a (first xs)) (member* a (rest xs))))))
+
+;; Next up: leftmost. The leftmost atom in the first list, if you recurse all the way down. But if the first item eventually resolves
+;;  to empty list, then "no answer"
+
+;; His examples do not cover the case where the 1st item is an atom. But his verbal description sounds like it should return that atom.
+;;  The interesting thing here is that, per his requirements, we don't actually recurse down the cdr! because the 1st item is either
+;;  an atom (we're done) or a list (which better ultimately have an atom). We could care less about the cdr. Hmm....
+
+(def leftmost
+  (fn [xs]
+    (cond
+     (null? xs) nil
+     (atom? (first xs)) (first xs)
+     :else (leftmost (first xs)))))
+
+;; Aha. He's saying that you are free to IGNORE the cases he tells you not to care about. As in "negative numbers DO NOT EXIST." In
+;;  this case, he's saying i can feel totally free to ignore empty lists. So let's rewrite it that way.
+
+(def leftmost-ignore-empty-lists
+  (fn [xs]
+    (cond
+     (atom? (first xs)) (first xs)
+     :else (leftmost (first xs)))))
+
+;; The only problem with this is invalid input will cause a stack overflow. In Scheme, not, because of tail call opt (assuming we
+;;  are careful to be in the tail position).
+
+;;
+;;
+;; Next up, eqlist? And he says NINE questions are needed. I only see about 7. So I'm going to write it and see if i find 9.
+;; I see, of course, that 3X3 is 9. But it seems to me some of those questions are unnecessary.
+;;
+;; If we follow his rule about "ask questions about 1 arg at a time," it will be verbose. Let's do it that way in case he wants it.
+
+(def eqlist?
+  (fn [xs ys]
+    (cond
+     (null? xs) (cond
+                 (null? ys) true
+                 :else false)
+     (atom? (first xs)) (cond
+                          (atom? (first ys)) (and (eqan? (first xs) (first ys)) (eqlist? (rest xs) (rest ys)))
+                          :else false)
+     :else (cond
+            (null? ys) false
+            (atom? (first ys)) false
+            :else (and (eqlist? (first xs) (first ys)) (eqlist? (rest xs) (rest ys)))))))
+
+;; To me, this could be shortened by allowing SOME of the questions to be asked about BOTH args at same time.
+;; Like this:
+
+(def eqlist-shorter?
+  (fn [xs ys]
+    (cond
+     (and (null? xs) (null? ys)) true
+     (and (atom? (first xs)) (atom? (first ys))) (and (eqan? (first xs) (first ys)) (eqlist-shorter? (rest xs) (rest ys)))
+     (or (atom? (first xs)) (atom? (first ys))) false
+     :else (and (eqlist-shorter? (first xs) (first ys)) (eqlist-shorter? (rest xs) (rest ys))))))
+
+
+;; Both appear to be correct and handle several cases in repl.
+;; Now, the 1st version asks 10 questions (surprise!) and the 2nd version asks 7 questions. Let's see what he has in mind...
+
+;; Sweet! on page 92, he has almost exactly what I have in my shorter version.
+
+;; So, when he first asks "how many questions", he really means, "What is the total number of all combinations." He also acknowledges
+;;  we may be able to logically simplify it from there.
+
+;; He says equal? (next up) needs 4 questions: is a an atom or list, and is b an atom or list. What he means is, 4 "cases"
+
+;; NOTE: Here was my first attempt. It BLOWS UP, because I never explicitly deal with empty list! rest returns empty list, and that
+;;  keeps going and going and going. Stackoverflow.
+
+;; (def equal?
+;;   (fn [a b]
+;;     (cond
+;;      (and (atom? a) (atom? b)) (eqan? a b)
+;;      (or (atom? a) (atom? b)) false
+;;      :else (and (equal? (first a) (first b)) (equal? (rest a) (rest b))))))
+
+(def equal?
+  (fn [a b]
+    (cond
+     (and (atom? a) (atom? b)) (eqan? a b)
+     (or (atom? a) (atom? b)) false
+     (null? a) (null? b) ;; Nice little shortcut here. If a is empty, then return whether or not b is empty.
+     (null? b) false
+     :else (and (equal? (first a) (first b)) (equal? (rest a) (rest b))))))
+
+;; WOW! He winds up with EXACTLY my WRONG ANSWER! The one with the stack overflow! If that doesn't show I'm on the right track,
+;;  I don't know what does! This must come down to some differences between Scheme and Clojure. Oh but wait ... he also delegates to
+;;  eqlist?, which I don't. So the difference is there, because eqlist? does handle empty list.
+
+;; Now he says rewrite eqlist? using equal?. What is the difference between the 2? eqlist? wants 2 lists, and it checks to see if
+;;  they are equal. equal will take any 2 SEXPs, meaning atoms OR lists, and it will see if they are equal. So equal? does everything
+;;  eqlist? does and more.
+
+(def eqlist-with-equal?
+  (fn [xs ys]
+    ;; So we will follow his approach of acting as if edge cases we don't support don't exist. So I won't even check to
+    ;;  see if they passed us lists. I will simple assume they did.
+    (equal? xs ys)))
+
+;; That was simple! In haskell, I'd just say eqlist-with-equal? = equal?. It'd just be some kind of alias.
+;; This is totally different from what he said, and I don't see why.
+
+;; Now things go a bit haywire. He says, "Here's rember". But what he presents is NOT rember. At best, it could be
+;;  "rember*", but it seems slightly different from that as well. Then he simplifies it.
+
+;; I think for now, I'll just focus on what he changes about the fn while simplifying it.
+;;
+;; OHH, WAIT, I GET IT! THE BOOK IS FINE. He is presenting a MODIFIED VERSION of rember that is capable of handling
+;;  not just lats, but actual lists of sexp's. He presents it fully formed (did not ask me to write it), and he asks me if
+;;  I can simplify it. Let's see.
+
+(def rember-for-sexp-simplified
+  (fn [sexp xs]
+    (cond
+     (null? xs) ()
+     :else (cond
+            (equal? (first xs) sexp) (rest xs)
+            :else (cons (first xs) (rember-for-sexp-simplified sexp (rest xs)))))))
+
+;; nailed it!!!
+
+;; Then he asks, "is rember a * fn now?". I think the answer is yes, but he says no. wassup? I do see that we only recur down the cdr.
+;;  But, we do recur there. And that's what fooled me: I tried some stuff where the sexp was buried deep in the cdr, and it worked.
+;;  Then I tried some stuff where the sexp WAS the entire car, and it worked. But I did NOT try having the sexp buried deep in the
+;;  car. So lemme try that (and I'll find that it does not work).
+;; CONFIRMED, it did not work. So this is NOT a * fn.
+
+;; He next asks "can it be further simplified?" I only see 1 thing: any time you have a cond inside the else of another cond, you
+;;  at least MIGHT be able to promote some or its questions up to the top cond. Lemme try that.
+
+(def rember-for-sexp-simplified-even-more
+  (fn [sexp xs]
+    (println "in badfn. sexp is " sexp " and xs is " xs)
+    (null? xs) ()
+    (println "made it past null check")
+    (equal? (first xs) sexp) (rest xs)
+    (println "made it past equal check")
+    :else (cons (first xs) (rember-for-sexp-simplified-even-more sexp (rest xs)))))
+
+;; That looks right to me, but it gets a stack overflow error. Let's figure out why. Blows on '(1) '(1)
+;;
+;; (rember-for-sexp-simplified-even-more '(1) '(1))
+;; sexp = '(1) and xs = '(1)
+;; null? no
+;; (equal 1 '(1))?
+;; Calling? equal? a=1, b='(1)
+;; One is an atom, the other is not. So equal returns false.
+;; (cons 1 (rember-for-sexp-simplified-even-more '(1) ()))
+;; (cons 1 ())
+;; '(1)
+;; That is the right result. That's what my hand eval says I should get. But instead I get stackoverflow!
+;;
+;; Let's do it again.
+;;
+;; (rember-for-sexp-simplified-even-more '(1) '(1))
+;; sexp = '(1)
+;; xs = '(1)
+;; (null? xs) -- no
+;; (equal? (first xs) sexp)
+;; (equal? 1 '(1))
+;; a = 1 and b = '(1)
+;; both atoms? no
+;; either one an atom? Yes. So return false.
+;;
+;; (cons (first xs) (rember-for-sexp-simplified-even-more sexp (rest xs)))
+;; (cons 1 (rember-for-sexp-simplified-even-more '(1) ()))
+;; sexp = '(1) and xs = ()
+;; null? xs -- yes. Return ().
+;;
+;; (cons 1 ())
+;; '(1)
+;;
+;; Hey! It's getting the stackoverflow in atom?!! Wish I'd noticed that sooner. Lets see wassup.
+;; a = 1 and b = '(1)
+;;
+     ;; (and (atom? a) (atom? b)) (eqan? a b)
+     ;; (or (atom? a) (atom? b)) false
+
+;; (atom? 1) -- true
+;; (atom? '(1)) -- false. Working just fine
+;;
+;; So it's not that.
+;; Let's do some println debugging!
+;;
+;; So atom? is fine. The stackoverflow can hit atr random places, just when you run out of stack space.
+
+;; At this point, there's nothing to do but walk away. I have no idea what the fuck is going on. It checks to see if () is an empty
+;;  list, and it gets false! And for DAYS now, that check has been working. I changed it to use (seq xs) because that's idiomatic
+;;  clojure, but it STILL fails.
+;;
+;; Maybe I'll see the problem in the morning.
